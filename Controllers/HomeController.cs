6bb83c3dc31a8d54b1cde20f0ef2c9f7db215975
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-06 15:59:02 -0500

    Validando función POST al enviar formulario

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,17 @@ namespace Portafolio.Controllers {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Contacto(ContactoModel contacto) {
+
+
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ Models/ContactoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Portafolio.Models {
+    public class ContactoModel {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
